a04ad69af040ad26ffa814f713d532bb872d47bc
yeonjinnk <165971416+yeonjinnk@users.noreply.github.com>
2024-07-08 11:21:51 +0900

    Add MemberController

---
+++ GIT_Test_KYJ/src/main/java/com/itwillbs/gittest/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.itwillbs.gittest.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
